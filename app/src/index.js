e06d6e1ad2c16456b0682f180dfa59f7d0c4d9b6
wdcunha@gmail.com <wdcunha@gmail.com>
2019-03-28 12:43:26 +0000

    Created component, changed index files

---
+++ app/src/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+// class App extends React.Component { // this will be changed by putting in import option above
+class App extends React.Component {
+  render() {
+    return <button>Enter</button>
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"))
