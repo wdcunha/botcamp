a234034dd0760cfa5ef04c7131d68af5c874c166
wdcunha@gmail.com <wdcunha@gmail.com>
2019-03-28 13:27:18 +0000

    Created App.css and js

---
+++ app/src/App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import "./App.css"
+
+class App extends React.Component {
+  render() {
+    return <button className="btn-botcamp">Enter</button>
+  }
+}
+
+export default App                        // this is responsible to make this class available for outside, namelly index.js from src
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
-// class App extends React.Component { // this will be changed by putting in import option above
-class App extends React.Component {
-  render() {
-    return <button>Enter</button>
-  }
-}
+import App from "./App"                                 // call the class that was moved, important to make it available here
+// class App extends React.Component {                  // this will be changed by putting in import option above
+// class App extends React.Component {                  // moved to app.js because index is not responsible for this
+//   render() {
+//     return <button>Enter</button>
+//   }
+// }
 ReactDOM.render(<App />, document.getElementById("root"))
